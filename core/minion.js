86e338cdd466f8a78df1917933fe1ad370d46a06
fengb <contact@fengb.info>
2015-01-06 22:38:24 -0600

    Basic minions!

--- core/minion.js
+++ core/minion.js
@@ -1,1 +1,16 @@
-module.exports = {}
+var Minion = module.exports = function(options){
+  this.init(options)
+}
+
+var proto = Minion.prototype
+
+proto.init = function(options){
+  options = options || {}
+  this.energy = options.energy || 100
+  this.lastTick = options.lastTick || +new Date()
+}
+
+proto.tick = function(time){
+  this.energy = 0
+  this.lastTick = time
+}
--- test/core/minion.js
+++ test/core/minion.js
@@ -1,9 +1,30 @@
 var expect = require('chai').expect
 
-var minion = require('cate/core/minion')
+var Minion = require('cate/core/minion')
 
-describe('minion', function(){
-  it('exists', function(){
-    expect(minion).to.be.ok()
+describe('Minion', function(){
+  beforeEach(function(){
+    this.minion = new Minion()
+  })
+
+  it('starts with 100 energy', function(){
+    expect(this.minion.energy).to.equal(100)
+  })
+
+  describe('#tick', function(){
+    beforeEach(function(){
+      this.now = this.minion.lastTick
+    })
+
+    it('sets lastTick', function(){
+      var newTime = this.now + 92085
+      this.minion.tick(newTime)
+      expect(this.minion.lastTick).to.equal(newTime)
+    })
+
+    it('removes all energy within an hour', function(){
+      this.minion.tick(this.now + 1000)
+      expect(this.minion.energy).to.equal(0)
+    })
   })
 })
