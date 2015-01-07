40f90c12634feee539097569b06da4efce80707d
fengb <contact@fengb.info>
2015-01-06 21:50:37 -0600

    require('jshint')

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp')
+var jshint = require('gulp-jshint')
 var mocha = require('gulp-mocha')
+
+gulp.task('jshint', function(){
+  return gulp.src(['*.js', '*.json'])
+           .pipe(jshint())
+           .pipe(jshint.reporter('default'))
+})
 
 gulp.task('mocha', function(){
   return gulp.src('test/**/*.js')
            .pipe(mocha())
-});
+})
