f0c883d87d69db9c45184fbe1d2c3a7596bf06ec
fengb <contact@fengb.info>
2015-01-06 22:23:26 -0600

    Gulp watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,16 +2,25 @@ var gulp = require('gulp')
 var jshint = require('gulp-jshint')
 var mocha = require('gulp-mocha')
 
+var paths = {
+  lint: [
+    '*.js', '*.json',
+    'browser/**/*.js',
+    'core/**/*.js',
+    'server/**/*.js',
+    'test/**/*.js',
+  ],
+  mocha: 'test/**/*.js',
+}
+
 gulp.task('lint', function(){
-  return gulp.src([
-             '*.js', '*.json',
-             'browser/**/*.js',
-             'core/**/*.js',
-             'server/**/*.js',
-             'test/**/*.js',
-           ])
+  return gulp.src(paths.lint)
            .pipe(jshint())
            .pipe(jshint.reporter('default'))
+})
+
+gulp.task('watch-lint', function(){
+  gulp.watch(paths.lint, ['lint'])
 })
 
 gulp.task('mocha', function(){
@@ -19,4 +28,10 @@ gulp.task('mocha', function(){
            .pipe(mocha())
 })
 
+gulp.task('watch-mocha', function(){
+  gulp.watch(paths.mocha, ['mocha'])
+})
+
 gulp.task('test', ['lint', 'mocha'])
+
+gulp.task('watch-test', ['watch-lint', 'watch-mocha'])
