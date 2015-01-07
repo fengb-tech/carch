c0339877cb411f0e8bcc96e3ea61e8e2d82adb07
fengb <contact@fengb.info>
2015-01-06 19:22:09 -0600

    require('browserify')

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express')
+var browserify = require('browserify-middleware')
 var app = express()
 
 app.set('view engine', 'jade')
 app.locals.basedir = app.get('views')
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res){
   res.render('home')
 })
+
+app.get('/game.js', browserify('./browser/game.js'))
 
 var port = Number(process.env.PORT || 5000)
 var server = app.listen(port, function (){
