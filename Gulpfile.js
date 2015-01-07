bd2bb66ca085643261c13d778b52a4a8a8c69767
fengb <contact@fengb.info>
2015-01-06 21:57:59 -0600

    Test task

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var jshint = require('gulp-jshint')
 var mocha = require('gulp-mocha')
 
-gulp.task('jshint', function(){
+gulp.task('lint', function(){
   return gulp.src(['*.js', '*.json'])
            .pipe(jshint())
            .pipe(jshint.reporter('default'))
@@ -12,3 +12,5 @@ gulp.task('mocha', function(){
   return gulp.src('test/**/*.js')
            .pipe(mocha())
 })
+
+gulp.task('test', ['lint', 'mocha'])
