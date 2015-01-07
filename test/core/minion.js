1de153d4365679e1fc7b189f04365d3d45c5187a
fengb <contact@fengb.info>
2015-01-06 21:24:21 -0600

    require('mocha', 'chai')

---
+++ test/core/minion.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect
+
+var minion = require('cate/core/minion')
+
+describe('minion', function(){
+  it('exists', function(){
+    expect(minion).to.be.ok()
+  })
+})
