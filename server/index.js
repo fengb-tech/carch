f0b4963bd5025908eb025766e87c9e6c5887c895
fengb <contact@fengb.info>
2015-01-06 20:27:51 -0600

    Moved most of server code

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var browserify = require('browserify-middleware')
-var app = express()
+var app = module.exports = express()
 
 app.set('view engine', 'jade')
 app.locals.basedir = app.get('views')
@@ -9,9 +9,4 @@ app.get('/', function(req, res){
   res.render('home')
 })
 
-app.get('/cate.js', browserify('./browser/cate.js'))
-
-var port = Number(process.env.PORT || 5000)
-var server = app.listen(port, function (){
-  console.log('CATE started on', port)
-})
+app.get('/cate.js', browserify('browser/cate.js'))
