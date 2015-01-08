2918966134e712ec1f0551f7b077501dd8ddbd05
fengb <contact@fengb.info>
2015-01-08 17:11:13 -0600

    require('pixi')

--- browser/carch.js
+++ browser/carch.js
@@ -1,1 +1,6 @@
-document.getElementById('carch').innerText = 'CARCH'
+var PIXI = require('pixi')
+
+var dom = document.getElementById('carch')
+var background = 0x66FF99
+var stage = new PIXI.Stage(background)
+var renderer = PIXI.autoDetectRenderer(512, 384, dom)
