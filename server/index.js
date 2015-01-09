154dfb83f98c983eafdd3ead484e9f6b14ff8002
fengb <contact@fengb.info>
2015-01-08 21:12:44 -0600

    Autoprefixer

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ var appRoot = require('app-root-path')
 var express = require('express')
 var browserify = require('browserify-middleware')
 var stylish = require('stylish')
+var autoprefixer = require('autoprefixer-stylus')
 
 var app = module.exports = express()
 
@@ -13,7 +14,12 @@ app.get('/', function(req, res){
   res.render('carch')
 })
 
-app.use(stylish({ src: appRoot + '/styles' }))
+app.use(stylish({
+  src: appRoot + '/styles',
+  setup: function(renderer) {
+    return renderer.use(autoprefixer())
+  }
+}))
 
 app.get('/carch.js', browserify(appRoot + '/browser/carch.js'))
 
