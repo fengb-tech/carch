89d4d699fbd1ea097f1921127d0fe89096e93231
fengb <contact@fengb.info>
2015-01-08 18:05:23 -0600

    Main loop/render

--- browser/carch.js
+++ browser/carch.js
@@ -3,6 +3,13 @@ var _ = require('lodash')
 
 var dom = document.getElementById('carch')
 var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, dom)
+var backgroundColor = 0x302000
+var stage = new PIXI.Stage(backgroundColor)
+requestAnimationFrame(function update(){
+  renderer.render(stage)
+
+  requestAnimationFrame(update)
+})
 
 window.addEventListener('resize', _.debounce(function(){
   renderer.resize(window.innerWidth, window.innerHeight)
