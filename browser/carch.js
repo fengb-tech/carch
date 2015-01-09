5f9a290b3f337257ba665e0520c8edeceb55ebda
fengb <contact@fengb.info>
2015-01-08 20:05:04 -0600

    Draw based on parent element instead of just the document

--- browser/carch.js
+++ browser/carch.js
@@ -2,7 +2,11 @@ var PIXI = require('pixi')
 var _ = require('lodash')
 
 var dom = document.getElementById('carch')
-var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, dom)
+var parentDom = dom.parentElement
+if(parentDom == document.body) {
+  parentDom = window
+}
+var renderer = PIXI.autoDetectRenderer(parentDom.width || parentDom.innerWidth, parentDom.height || parentDom.innerHeight, dom)
 var backgroundColor = 0x302000
 var stage = new PIXI.Stage(backgroundColor)
 requestAnimationFrame(function update(){
@@ -11,6 +15,6 @@ requestAnimationFrame(function update(){
   requestAnimationFrame(update)
 })
 
-window.addEventListener('resize', _.debounce(function(){
-  renderer.resize(window.innerWidth, window.innerHeight)
+parentDom.addEventListener('resize', _.debounce(function(){
+  renderer.resize(parentDom.width || parentDom.innerWidth, parentDom.height || parentDom.innerHeight)
 }, 200))
