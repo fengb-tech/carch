19fe40e96e9da33abc5e29bc5a721b27629d8195
fengb <contact@fengb.info>
2015-01-08 20:48:38 -0600

    Added stylus/stylish

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ var appRoot = require('app-root-path')
 
 var express = require('express')
 var browserify = require('browserify-middleware')
+var stylish = require('stylish')
+
 var app = module.exports = express()
 
 app.set('view engine', 'jade')
@@ -10,6 +12,8 @@ app.locals.basedir = app.get('views')
 app.get('/', function(req, res){
   res.render('carch')
 })
+
+app.use(stylish({ src: appRoot + '/styles' }))
 
 app.get('/carch.js', browserify(appRoot + '/browser/carch.js'))
 
